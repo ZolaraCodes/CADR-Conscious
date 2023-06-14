b959c57408d82356a26b98021a340e7240af9f4d
Rozalyn <96503223+ZolaraCodes@users.noreply.github.com>
2023-06-14 18:59:23 -0400

    Update calculator.jsx

--- src/calculator.jsx
+++ src/calculator.jsx
@@ -39,6 +39,7 @@ const AirQualityCalculator = () => {
   return (
     <div className="AirQualityCalculator">
       <h2>CADR Conscious</h2>
+    <details>This is an air quality calculator </details>
       <div>
         <label>
           ACH (Air Changes per Hour):
